1ac2aba3fcdb9af279a31971aa956814ab59df4c
azyrus66 <azyrus66@gmail.com>
2018-10-16 18:04:26 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,53 @@
-import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(nagybetu("kutya, cica, máté, gábor"));
+		System.out.println(tukorString("aefhfea"));
+		System.out.println(nemBetu("doj3iOcO8, did3Gjdo."));
+	}
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
+	public static String nagybetu(String kis) {
+		byte kisBytes[] = kis.getBytes();
 
-		Scanner input = new Scanner(System.in);
+		if ((kisBytes[0] >= 97) && (kisBytes[0] <= 122)) {
+			kisBytes[0] -= 32;
+		}
+		for (int i = 1; i <= kisBytes.length - 1; i++) {
+			if ((kisBytes[i] >= 97) && (kisBytes[i] <= 122) && (kisBytes[i - 1] == ' ')) {
+				kisBytes[i] -= 32;
+			}
+		}
+		String nagy = new String(kisBytes);
+		return nagy;
+	}
 
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
+	public static boolean tukorString(String srg) {
+		byte srgBytes[] = srg.getBytes();
+		int i = 0, j = srgBytes.length - 1;
+		while (true) {
+			if (srgBytes[i] != srgBytes[j]) {
+				return false;
+			}
+			else if (j - i <= 1) {
+				return true;
+			}
+			i++;
+			j--;
+		}
+	}
 
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
+	public static String nemBetu(String srg) {
+		byte srgBytes[] = srg.getBytes();
+		for (int i=0; i < srgBytes.length; i++) {
+			if (!(((srgBytes[i] >= 'A') && (srgBytes[i] <= 'Z')) || ((srgBytes[i] >= 'a') && (srgBytes[i] <= 'z')))) {
+				srgBytes[i] = ' ';
+			}
+		}
+		String s = new String(srgBytes);
+		return s;
+	}
 
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+	public static String olvas() {
+		String s = new String();
+		return s;
 	}
 }
